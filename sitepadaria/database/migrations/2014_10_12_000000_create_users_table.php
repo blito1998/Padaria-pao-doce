bf2fc4723671b358edfa1c78f55a69119aecf47c
Edson Augusto <edson6039@hotmail.com>
2021-09-05 01:08:41 -0300

    Admin Funcionario e Cliente

--- sitepadaria/database/migrations/2014_10_12_000000_create_users_table.php
+++ sitepadaria/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->char('Verificador', 1)->default='3';
         });
     }
 
