4175812547cff4bfd2350187a3449ae6e175def6
Edson Augusto <edson6039@hotmail.com>
2021-09-09 16:54:20 -0300

    admin e cliente

--- sitepadaria/routes/web.php
+++ sitepadaria/routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\PadariaController;
 
 Route::get('/',[PadariaController::class, 'index'] );
 Route::get('/pagecontato',[PadariaController::class, 'pagecontato'] );
-
 Route::get('/cadastro/carrinhocompras',[PadariaController::class, 'carrinhocompras']);
 Route::get('/cadastro',[PadariaController::class, 'cadastro'] )->middleware('auth');
 Route::get('/cadastro/cadastroprodutos',[PadariaController::class, 'cadastroprodutos'] );
@@ -15,9 +14,7 @@ Route::post('/cadastro',[PadariaController::class, 'store']);
 Route::delete('/cadastro/{id}',[PadariaController::class,'destroy'])->middleware('auth');
 Route::get('/cadastro/edit/{id}',[PadariaController::class,'edit'])->middleware('auth');
 Route::put('/cadastro/update/{id}',[PadariaController::class,'update'])->middleware('auth');
-
 Route::get('/dashboard',[PadariaController::class, 'dashboard'])->middleware('auth');
-
 Route::delete('/cadastro/leave/{id}',[PadariaController::class, 'leavecarrinho'])->middleware('auth');
 Route::get('/cadastro/carrinho/{id}',[PadariaController::class, 'carrinho'])->middleware('auth');
 
