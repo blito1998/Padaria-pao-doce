ab55f6c9e68351ded01165b3e0a01d2f07ce6f83
Edson Augusto <edson6039@hotmail.com>
2021-09-09 13:29:19 -0300

    Layout paginas de autenticacao

--- sitepadaria/resources/views/auth/passwords/confirm.blade.php
+++ sitepadaria/resources/views/auth/passwords/confirm.blade.php
@@ -1,20 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header">{{ __('Confirmar senha') }}</div>
 
                 <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    {{ __('Por favor, confirme sua senha antes de continuar.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
